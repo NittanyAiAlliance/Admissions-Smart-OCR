7de6058b50d6e2101c1efb92b33fb6443935eeef
Joel Seidel <jds6294@psu.edu>
2019-11-12 15:15:36 -0500

    Log types and boilerplate

---
+++ api/src/main/types/ILoggable.java
@@ -0,0 +1,5 @@
+package main.types;
+
+public interface ILoggable {
+    Log toLog();
+}
---
+++ api/src/main/types/Log.java
@@ -0,0 +1,39 @@
+package main.types;
+
+import java.util.Date;
+
+public class Log {
+    private LogType type;
+    private String content;
+    private Date timestamp;
+
+    /**
+     * Default constructor - define type of log and set time stamp
+     */
+    public Log(){
+        this.type = LogType.TRANSACTION;
+        this.timestamp = new Date();
+    }
+
+    public LogType getType() {
+        return type;
+    }
+
+    public void addContent(String content) {
+        System.out.println(content);
+        this.content += content;
+        this.content += this.content.equals("") ? ", " : "";
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp){
+        this.timestamp = timestamp;
+    }
+}
