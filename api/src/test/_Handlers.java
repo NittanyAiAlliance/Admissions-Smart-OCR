b656009a3b13707441cceed1046d31cb18c156ed
Joel Seidel <joeldseidel@gmail.com>
2019-12-10 13:47:27 -0500

    Added new handler test

---
+++ api/src/test/_Handlers.java
@@ -0,0 +1,28 @@
+package test;
+
+import main.handlers.FilePostHandler;
+import main.handlers.TokenRequestHandler;
+import static org.junit.Assert.*;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class _Handlers {
+    private static final String test_AWTString = "BW3n+Wyt7N1/5ijHQGsDXtVH2Xut36jNVqpFaTqfzQ8=";
+    @Test
+    public void requestToken(){
+        System.out.println("Testing token request");
+        TokenRequestHandler tokenRequestHandler = new TokenRequestHandler();
+        tokenRequestHandler.fulfillRequest(new JSONObject());
+        assertNotEquals("", tokenRequestHandler.getResponse());
+        JSONObject requestResponse = new JSONObject(tokenRequestHandler.getResponse());
+        assertTrue(requestResponse.getBoolean("success"));
+        assertEquals(test_AWTString, requestResponse.getString("token"));
+    }
+    @Test
+    public void filePost(){
+        System.out.println("Testing file post request");
+        FilePostHandler filePostHandler = new FilePostHandler();
+
+    }
+}
