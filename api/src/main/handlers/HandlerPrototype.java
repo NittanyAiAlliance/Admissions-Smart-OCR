761692aa2598a441b0a367a4037f72b55f145811
Joel Seidel <jds6294@psu.edu>
2020-01-23 11:12:58 -0500

    Remove jwt dependency

--- api/src/main/handlers/HandlerPrototype.java
+++ api/src/main/handlers/HandlerPrototype.java
@@ -1,9 +1,5 @@
 package main.handlers;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import main.managers.LogManager;
