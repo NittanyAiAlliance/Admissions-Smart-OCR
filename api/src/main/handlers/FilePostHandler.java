e7c9cf8f204c3a88a0da6b2bb9e2e3a0f245373a
Joel Seidel <jds6294@psu.edu>
2020-02-06 17:35:20 -0500

    Removed write to file functionality

--- api/src/main/handlers/FilePostHandler.java
+++ api/src/main/handlers/FilePostHandler.java
@@ -59,53 +59,6 @@ public class FilePostHandler extends HandlerPrototype implements HttpHandler {
         }
     }
 
-    private final String uploadTempFilePath = System.getProperty("user.home") + "/upload-temp";
-
-    /**
-     * Write the image base 64 string to a temp file
-     * @param imgStr image base 64 string
-     * @return name of the file the image data was written into
-     */
-    private String writeImageToFile(String imgStr){
-        //Get the image base 64 encoding and convert to image byte array
-        byte[] imgData = Base64.getDecoder().decode(imgStr.split(",")[1]);
-        String dataFile = uploadTempFilePath + "/" + getTempFileRandomName();
-        Path tempFile = Paths.get(dataFile);
-        try {
-            Files.createFile(tempFile);
-            System.out.println("Creating file: " + dataFile);
-        } catch (FileAlreadyExistsException faeEx){
-            //In the extremely unlikely event of a random file name collision, make another name
-            //This will happen 1 out of every 4.76 * 10^28 iterations
-            dataFile = uploadTempFilePath + "/" + getTempFileRandomName();
-            tempFile = Paths.get(dataFile);
-        } catch (IOException ioEx){
-            ioEx.printStackTrace();
-        }
-        try {
-            //Write image data to a randomly named temporary file
-            Files.write(tempFile, imgData);
-        } catch (IOException ioEx) {
-            ioEx.printStackTrace();
-        }
-        return dataFile;
-    }
-
-    /**
-     * Get a random name for the temporary file
-     * @return random name for temp file
-     */
-    private String getTempFileRandomName() {
-        String saltChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        StringBuilder salt = new StringBuilder();
-        Random rnd = new Random();
-        while (salt.length() < 16) {
-            int index = (int) (rnd.nextFloat() * saltChars.length());
-            salt.append(saltChars.charAt(index));
-        }
-        return salt.toString() + ".bin";
-    }
-
     @Override
     public Log toLog(){
         return new Log();
