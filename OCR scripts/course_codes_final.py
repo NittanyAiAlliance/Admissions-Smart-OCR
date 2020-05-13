4ae56579181e22376a92dc99fabf6e7dd8852740
Joel Seidel <jds6294@psu.edu>
2020-05-13 11:19:56 -0400

    OCR generic class script

---
+++ OCR scripts/course_codes_final.py
@@ -0,0 +1,30 @@
+from fuzzywuzzy import process
+import pandas as pd
+import os
+import sys
+import json
+def course_codes(input):
+    df = pd.ExcelFile("subject_code.xlsx")
+    df1 = pd.read_excel(df,"Course Codes")
+    arr = df1['Unnamed: 2'].values.tolist()
+    with open(input) as f:
+        data = json.load(f)
+
+    for i in data:
+        try:
+            if (process.extractOne(data[i]['COURSE'], arr)[1] > 75):
+
+                data[i]['COURSE_NAME'] = process.extractOne(data[i]['COURSE'], arr)[0]
+            else:
+                data[i]['COURSE_NAME'] = "Others"
+        except:
+            data[i]['COURSE_NAME'] = "None"
+            pass
+
+    with open('final.json', 'w') as outfile:
+        json.dump(data, outfile)
+if __name__ == "__main__":
+    relative_path = sys.argv[1]
+
+    if os.path.exists(relative_path):
+        course_codes(os.path.abspath(relative_path))
