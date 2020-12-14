1c60838d0cfe9c52be6b103fc4ad1a1930cc0a1b
Harsh Gupta <43788815+harshg28101999@users.noreply.github.com>
2020-12-14 15:29:06 -0800

    School Info Training Data

---
+++ OCR scripts/school-training-data.py
@@ -0,0 +1,49 @@
+TRAIN_SCHOOL_DATA =[
+
+    # https://psu.app.box.com/file/615034288932
+    # Upper left table 
+    ("Warwick School District", {"entities": [(0, 7, "EXTRA"), (8, 14, "EXTRA"), (15, 22, "EXTRA")]}),
+    ("Warwick High School", {"entities": [(0, 7, "SCHOOL_NAME"), (8, 12, "SCHOOL_NAME"), (13, 18, "SCHOOL_NAME")]}),
+    ("301 W Orange St", {"entities": [(0, 4, "ADDRESS"), (5, 6, "ADDRESS"), (15, 22, "ADDRESS"), (15, 22, "ADDRESS")]}),
+    ("Lititz, PA 17543", {"entities": [(0, 7, "CITY"), (8, 10, "STATE"), (11, 15, "ZIP_CODE")]}),
+    ("(717)626-3700", {"entities": [(0, 12, "EXTRA")]}),
+    ("Official Student Transcript", {"entities": [(0, 26, "EXTRA")]}),
+
+
+    # https://psu.app.box.com/file/615025686483
+    # Upper Panel with the school details
+    ("Student Transcript -- State College Area School District", {"entities": [(0, 7, "EXTRA"), (8, 17, "EXTRA"), (18, 21, "EXTRA"), (22, 27, "EXTRA"),(28, 35, "EXTRA"), (36, 40, "EXTRA"), (41, 47, "EXTRA"), (48, 55, "EXTRA")]}),
+    ("Hipp, Alessia Sofia", {"entities": [(0, 5, "EXTRA"), (6, 13, "EXTRA"), (14, 18, "EXTRA")]}),
+    ("766 Tanager DR State College Area HS", {"entities": [(0, 3, "EXTRA"), (4, 11, "EXTRA"), (12, 14, "EXTRA"), (15, 20, "SCHOOL_NAME"), (21, 28, "SCHOOL_NAME"), (29, 33, "SCHOOL_NAME"), (34, 35, "SCHOOL_NAME")]}),
+    ("State College, PA 16803 650 Westerly Pkwy", {"entities": [(0, 5, "EXTRA"), (6, 14, "EXTRA"), (15, 17, "EXTRA"), (18, 23, "EXTRA"), (24, 27, "ADDRESS"), (28, 36, "ADDRESS"), (37, 40, "ADDRESS")]}),
+    ("State College, PA 16801", {"entities": [(0, 5, "CITY"), (6, 14, "CITY"), (15, 17, "STATE"), (18, 22, "ZIP_CODE")]}),
+    ("(814)231-1130 CEEB Code: 394685", {"entities": [(0, 12, "EXTRA"), (13, 18, "EXTRA"), (19, 24, "EXTRA"), (25, 30, "CEEB_CODE")]}),
+
+
+    # https://psu.app.box.com/file/615034315551
+    # Top Panel with school details
+    ("6/24/2019 Student Transcript Page 1 of 1", {"entities": [(0, 9, "EXTRA"), (9, 17, "EXTRA"), (18, 28, "EXTRA"), (29, 33, "EXTRA"), (34, 39, "EXTRA")]}),
+    ("To: Parker Christian Brandt From: North Penn High School", {"entities": [(0, 3, "EXTRA"), (4, 9, "EXTRA"), (10, 20, "EXTRA"), (21, 27, "EXTRA"), (28, 33, "EXTRA"), (34, 39, "SCHOOL_NAME"), (40, 44, "SCHOOL_NAME"), (45, 49, "SCHOOL_NAME"), (50, 55, "SCHOOL_NAME")]}),
+    ("1625 Morris Rd 1340 Valley Forge Rd Mr. Pete Nicholson", {"entities": [(0, 4, "EXTRA"), (5, 11, "EXTRA"), (12, 14, "EXTRA"), (15, 19, "ADDRESS"), (20, 26, "ADDRESS"), (27, 32, "ADDRESS"), (33, 35, "ADDRESS"), (36, 39, "EXTRA"), (40, 44, "EXTRA"), (45, 53, "EXTRA")]}),
+    ("Lansdale, PA 19446 Lansdale, PA 19446 (215) 368-9800", {"entities": [(0, 9, "EXTRA"), (10, 12, "EXTRA"), (13, 18, "EXTRA"), (19, 28, "CITY"), (29, 31, "STATE"), (32, 37, "ZIP_CODE"), (38, 51, "EXTRA")]}),
+
+
+    # https://psu.app.box.com/file/473839925398
+    # 2nd panel from the top
+    ("September 09, 2016", {"entities": [(0, 9, "EXTRA"), (10, 13, "EXTRA"), (14, 17, "EXTRA")]}),
+    ("North Alleghany School District", {"entities": [(0, 5, "SCHOOL_NAME"), (6, 15, "SCHOOL_NAME"), (16, 22, "SCHOOL_NAME"), (23, 30, "SCHOOL_NAME")]}),
+    ("10375 Perry Hwy Official Transcript Of ", {"entities": [(0, 5, "ADDRESS"), (6, 11, "ADDRESS"), (12, 15, "ADDRESS"), (16, 24, "EXTRA"), (25, 35, "EXTRA"), (36, 38, "EXTRA")]}),
+    ("Wexford, PA 15090", {"entities": [(0, 8, "CITY"), (9, 11, "STATE"), (12, 16, "ZIP_CODE")]}),
+    ("724 935-5767", {"entities": [(0, 11, "EXTRA")]}),
+    ("Accredited by the Middle States Association", {"entities": [(0, 10, "EXTRA"), (11, 13, "EXTRA"), (14, 17, "EXTRA"), (18, 24, "EXTRA"), (25, 31, "EXTRA"), (32, 42, "EXTRA")]}),
+    ("of Secondary Schools", {"entities": [(0, 2, "EXTRA"), (3, 11, "EXTRA"), (12, 19, "EXTRA")]}),
+    ("CEEB# 393745", {"entities": [(0, 5, "EXTRA"), (6, 11, "CEEB_CODE")]}),
+
+
+    # https://psu.app.box.com/file/473822428829
+    # Top panel
+    ("Aug 25 2016 MI", {"entities": [(0, 3, "EXTRA"), (4, 6, "EXTRA"), (7, 11, "EXTRA"), (12, 13, "EXTRA")]}),
+    ("8/17/2016 Student Transcript Page 1 of 1", {"entities": [(0, 9, "EXTRA"), (10, 17, "EXTRA"), (18, 28, "EXTRA"), (29, 33, "EXTRA"), (34, 35, "EXTRA"), (36, 37, "EXTRA"), (3, 9, "EXTRA")]}),
+    ("To: From: Graduate Building", {"entities": [(0, 3, "EXTRA"), (4, 9, "EXTRA"), (10, 18, "SCHOOL_NAME"), (19, 26, "EXTRA")]}),
+    ("PA 17033", {"entities": [(0, 2, "STATE"), (3, 7, "ZIP_CODE")]}),
+]
