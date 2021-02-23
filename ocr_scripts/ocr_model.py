99f322fbdd16567a8aa65b4644e6ff3128d49289
J. Famous <joshua.famous@gmail.com>
2021-02-23 13:25:27 -0500

    Production API, low accuracy

--- ocr_scripts/ocr_model.py
+++ ocr_scripts/ocr_model.py
@@ -17,26 +17,24 @@ from course_codes_final import course_codes
 
 def process_ocr(csv_data):
     # selects an existing model or creates a new model
-    nlp = spacy.load(Path(os.getcwd()+'/model/model-last'))  # load existing spaCy model
+    nlp = spacy.load(Path(os.getcwd()+'/model/model-best'))  # load existing spaCy model
     ruler = nlp.add_pipe("attribute_ruler")
     patterns = [[{"ORTH": "	"}]]
     attrs = {"TAG": "TAB", "POS": "PUNCT"}
     ruler.add(patterns=patterns, attrs=attrs)
-    print("Loaded model '%s'" % Path(os.getcwd()+'/model/model-last'))
+    print("Loaded model '%s'" % Path(os.getcwd()+'/model/model-best'))
 
-    out_dict = {}
-    for index, text in enumerate(csv_data):
-        print('Processing ' + text)
-        text = ''.join(line).replace('"', '').replace(',', '').rstrip('\n')
-
-        doc = nlp(text)
-
+    lines = {}
+    for index, text in enumerate(csv_data.split('\n')):
+        text = ''.join(text).replace('"', '').replace(',', '').rstrip('\n')
+        if len(text) > 0:
+            doc = nlp(text)
+            for token in doc:
+                print(str(token.i) + ": " + token.text)
+            this_course = [doc.text]
+            for ent in doc.ents:
+                this_ent = (ent.label_, ent.text)
+                this_course.append(this_ent)
+            lines["Line " + str(index)] = this_course
         
-
-        linedict = {}
-        for ent in doc.ents:
-            print('Doc ' + str(ent) + ' has label ' + str(ent.label_))
-            linedict[ent.label_] = ent.text
-        out_dict["line "+str(ind)] = linedict
-        
-    return out_dict
+    return lines
