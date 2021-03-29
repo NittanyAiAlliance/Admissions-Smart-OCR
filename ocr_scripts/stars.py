6294179ae1f86fb173735de7ce06ac911f5ab222
J. Famous <joshua.famous@gmail.com>
2021-03-29 12:16:42 -0400

    Course codes via fuzzywuzzy

--- ocr_scripts/stars.py
+++ ocr_scripts/stars.py
@@ -1,12 +1,8 @@
 from fuzzywuzzy import process
-from difflib import SequenceMatcher
 import pandas as pd
 import os
 import sys
 import json
-
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
 
 def process_stars(data):
     df = pd.ExcelFile("subject_code.xlsx")
@@ -21,27 +17,17 @@ def process_stars(data):
                 line_course = label[1]
         # Ignore lack of course label
         if(len(line_course) > 0):
-            #try:
+            try:
                 # Process generic course title similarity with fuzzywuzzy
-                #similar_course = process.extractOne(line_course, generic_course_titles)
-                #if (similar_course[1] > 75):
-                #    data[line].append(["COURSE_NAME", similar_course[0]])
-                #    print("Returning " + line_course + " " + str(similar_course[1]) + " perct similar to " + similar_course[0])
-                #else:
-                #    data[line].append(["COURSE_NAME", "OTHER"])
-            closest = 0
-            course = ""
-            for star in generic_course_titles:
-                if(len(str(star)) > 0):
-                    sim = similar(line_course, str(star))
-                    if(sim > closest):
-                        closest = sim
-                        course = str(star)
-            print("Returning " + line_course + " " + str(closest) + " perct similar to " + course)
-            data[line].append(["COURSE_NAME", course])
-            #except:
-            #    data[line].append(["COURSE_NAME", "ERROR"])
-            #    pass
+                similar_course = process.extractOne(line_course, generic_course_titles)
+                if (similar_course[1] > 75):
+                    data[line].append(["COURSE_NAME", similar_course[0]])
+                    print("Returning " + line_course + " " + str(similar_course[1]) + " perct similar to " + similar_course[0])
+                else:
+                    data[line].append(["COURSE_NAME", "OTHER"])
+            except:
+                data[line].append(["COURSE_NAME", "ERROR"])
+                pass
         else:
             data[line].append(["COURSE_NAME", "NONE"])
 
