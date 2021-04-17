17c55e8c68bd98f584fc3b8bf97104ac1b72b078
Joel Seidel <joeldseidel@gmail.com>
2021-04-17 10:06:20 -0400

    Update main to contain store

--- ui/smart_ocr/src/main.js
+++ ui/smart_ocr/src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material/'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import Store from "./store";
 
 Vue.config.productionTip = false
 
@@ -16,5 +18,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
+  store: Store,
   template: '<App/>'
 })
