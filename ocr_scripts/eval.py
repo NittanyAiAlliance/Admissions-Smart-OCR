a8befacbf73d9aa0a35a90cd10f7ce79d409f12d
J. Famous <joshua.famous@gmail.com>
2021-05-10 15:34:16 -0400

    Subject code update, course code acc

--- ocr_scripts/eval.py
+++ ocr_scripts/eval.py
@@ -14,7 +14,7 @@ EVAL_DATA = np.load(fp, allow_pickle=True).tolist()
 totals = {"GRADE": 0, "COURSE": 0, "LEVEL":0, "CREDIT":0, "EXTRA": 0, "COURSE_CODE": 0}
 hits = {"GRADE": 0, "COURSE": 0, "LEVEL":0, "CREDIT":0, "EXTRA": 0, "COURSE_CODE": 0}
 accuracy = {"GRADE": 0, "COURSE": 0, "LEVEL":0, "CREDIT":0, "EXTRA": 0, "COURSE_CODE": 0}
-proximity = {"GRADE": 0, "COURSE": 0, "LEVEL":0, "CREDIT":0, "EXTRA": 0}
+proximity = {"GRADE": 0, "COURSE": 0, "LEVEL":0, "CREDIT":0, "EXTRA": 0, "COURSE_CODE": 0}
 synopsis = 0
 
 # Function to find similarity between two strings
@@ -39,8 +39,6 @@ def eval():
             if(this_label != 'EXTRA' and this_label != 'COURSE_SUBJECT' and this_label != 'COURSE_NAME'):
                 hit = False
                 for entry in range(1, len(line)): # Iterate returned labels
-                    if(this_label == 'COURSE_CODE'):
-                        print('Comparing ' + line[entry][0] + ' to ' + this_label + ' and ' + line[entry][1] + ' to ' + this_text)
                     if(line[entry][0] == this_label and line[entry][1] == this_text): # Return hit on exact match
                         inc_hits(this_label)
                         hit = True
--- ocr_scripts/stars.py
+++ ocr_scripts/stars.py
@@ -1,9 +1,11 @@
 from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
 import pandas as pd
 import os
 import sys
 import json
 import spacy
+import difflib
 
 # Configuration for course codes excel file
 fp = pd.ExcelFile("subject_code.xlsx")
@@ -22,28 +24,29 @@ generic_course_titles = df[course_col].values.tolist()
 def process_stars(data):
     
     for line in data:
+        line_course = data[line][0]
         # Search ocr result for course label
-        line_course = ""
-        for label in data[line][1:]:
-            if(label[0] == "COURSE"):
-                line_course = label[1]
+        #for label in data[line][1:]:
+        #    if(label[0] == "COURSE"):
+        #        line_course = label[1]
         # Ignore lack of course label
         if(len(line_course) > 0):
-            #try:
-            # Process generic course title similarity with fuzzywuzzy
-            similar_course = process.extractOne(line_course, generic_course_titles)
-            if (similar_course[1] > 75):
-                # Add complete STARS info to return record
-                codes = find_code(similar_course[0])
-                data[line].append(["COURSE_NAME", similar_course[0]])
-                data[line].append(["COURSE_SUBJECT", codes[0]])
-                data[line].append(["COURSE_CODE", codes[1]])
-                print("Returning " + line_course + " " + str(similar_course[1]) + " perct similar to " + similar_course[0])
-            else:
-                data[line].append(["COURSE_NAME", "OTHER"])
-            #except:
-            #    data[line].append(["COURSE_NAME", "ERROR"])
-            #    pass
+            line_course = lemmatizer(line_course)
+            try:
+                # Process generic course title similarity with fuzzywuzzy
+                similar_course = process.extractOne(line_course, generic_course_titles, scorer=fuzz.partial_ratio)
+                if (similar_course[1] > 75):
+                    # Add complete STARS info to return record
+                    codes = find_code(similar_course[0])
+                    data[line].append(["COURSE_NAME", similar_course[0]])
+                    data[line].append(["COURSE_SUBJECT", codes[0]])
+                    data[line].append(["COURSE_CODE", codes[1]])
+                    #print("Returning " + line_course + " " + str(similar_course[1]) + " perct similar to " + similar_course[0])
+                else:
+                    data[line].append(["COURSE_NAME", "OTHER"])
+            except:
+                data[line].append(["COURSE_NAME", "ERROR"])
+                pass
             
             # Process generic course title similarity with spacy vectors
             #line_course_doc = nlp(line_course)
@@ -60,9 +63,75 @@ def process_stars(data):
         else:
             data[line].append(["COURSE_NAME", "NONE"])
 
-    print('Returning stars on ' + str(data))
-
     return data
+
+# Function to test comparators
+def test_compare():
+    courses = ["US History", "United States History", "Calculus BC", "Calc BC", "Calculus (BC)"]
+    for course in courses:
+        course = lemmatizer(course)
+        print('For ' + course + str(difflib.get_close_matches(course, generic_course_titles, n=1, cutoff=0.2)))
+        answer = process.extractOne(course, generic_course_titles, scorer=fuzz.partial_ratio)
+        print("Returning " + course + " " + str(answer[1]) + " perct similar to " + answer[0])
+
+# Lemmatize synonymous strings to STARS compatibility
+def lemmatizer(line):
+    course_dict = {
+        'mgmt': 'management',
+        'hr': 'human resources',
+        'bus': 'business',
+        'acc': 'accounting',
+        'acct': 'accounting',
+        'cs': 'computer science',
+        'eng': 'english',
+        'engl': 'english',
+        'comp': 'composition',
+        'lit': 'literature',
+        'hist': 'history',
+        'orch': 'orchestra',
+        'photo': 'photography',
+        'vid': 'video',
+        'lang': 'language',
+        'alg': 'algebra',
+        'trig': 'trigonometry',
+        'stat': 'statistics',
+        'precalc': 'precalculus',
+        'prog': 'programming',
+        'geom': 'geometry',
+        'diffeq': 'differential equations',
+        'calc': 'calculus',
+        'sci': 'science',
+        'env': 'environment',
+        'chem': 'chemistry',
+        'bio': 'biology',
+        'civ': 'civilization',
+        'united': 'us',
+        'states': '',
+        'psych': 'psychology',
+        'euro': 'european',
+        'gov': 'government',
+        'poli': 'polotics',
+        'geog': 'geography',
+        'econ': 'economics',
+        '1': '9',
+        '2': '10',
+        '3': '11',
+        '4': '12',
+        'pe': 'physical education',
+        'i': '1',
+        'ii': '2',
+        'iii': '3',
+        'iv': '4',
+        'v': '5',
+        'vi': '6'
+    }
+    line = line.split(' ')
+    index = 0
+    for token in line:
+        if(token.lower() in course_dict):
+            line[index] = course_dict[token.lower()]
+        index += 1
+    return ' '.join(line)
 
 # Function to find subject and course code given STARS generic course name
 def find_code(title):
